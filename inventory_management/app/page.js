11837109000b5993bac89b0ab93a4662dcdd6bff
Manyaarya <manyam.8227@gmail.com>
2024-08-07 20:34:35 +0530

    Added remove quantity function

--- inventory_management/app/page.js
+++ inventory_management/app/page.js
@@ -22,6 +22,22 @@ export default function Home() {
     setInventory(inventoryList)
   }
 
+ const removeItem = async (item) => {
+  const docRef = doc(collection(firestone, 'inventory'), item)
+  const docSnap = await getDoc(docRef)
+
+  if(docSnap.exists()) {
+    const {quantity} = docSnap.data()
+    if(quantity === 1) {
+      await deleteDoc(docRef)
+    }
+    else{
+      await setDoc(docRef, {quantity: quantity - 1})
+    }
+  }
+  await updateInventory()
+ }
+
   useEffect(() => {
     updateInventory()
   }, [])
